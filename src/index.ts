1445e1a4d0ce0a58b6f88f64e41177472057eb03
Hency Dsouza <hencyhd39@gmail.com>
2025-02-01 19:06:03 +0530

    feat: Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { config } from "./config/app.config";
+
+const app = express();
+// const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: config.APP_ORIGIN,
+        credentials: true,
+    })
+)
+
+app.use(cookieParser());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Hello World!"
+    });
+})
+
+app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+})
